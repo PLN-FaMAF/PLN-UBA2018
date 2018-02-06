b840f9bb8c54724d35cd06383548524db222d3c0
Franco M. Luque <francolq@famaf.unc.edu.ar>
2018-02-05 21:53:04 -0300

    training script from 2017.

---
+++ languagemodeling/scripts/train.py
@@ -0,0 +1,34 @@
+"""Train an n-gram model.
+
+Usage:
+  train.py -n <n> -o <file>
+  train.py -h | --help
+
+Options:
+  -n <n>        Order of the model.
+  -o <file>     Output model file.
+  -h --help     Show this screen.
+"""
+from docopt import docopt
+import pickle
+
+from nltk.corpus import gutenberg
+
+from languagemodeling.ngram import NGram
+
+
+if __name__ == '__main__':
+    opts = docopt(__doc__)
+
+    # load the data
+    sents = gutenberg.sents('austen-emma.txt')
+
+    # train the model
+    n = int(opts['-n'])
+    model = NGram(n, sents)
+
+    # save it
+    filename = opts['-o']
+    f = open(filename, 'wb')
+    pickle.dump(model, f)
+    f.close()
