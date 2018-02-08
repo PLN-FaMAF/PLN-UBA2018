2cc1251132d73e8e66fe5e24d96206c9c9057c3c
Franco M. Luque <francolq@famaf.unc.edu.ar>
2018-02-08 15:06:51 -0300

    More help on scripts.

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -1,11 +1,15 @@
 """Train an n-gram model.
 
 Usage:
-  train.py -n <n> -o <file>
+  train.py [-m <model>] -n <n> -o <file>
   train.py -h | --help
 
 Options:
   -n <n>        Order of the model.
+  -m <model>    Model to use [default: ngram]:
+                  ngram: Unsmoothed n-grams.
+                  addone: N-grams with add-one smoothing.
+                  inter: N-grams with interpolation smoothing.
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
@@ -15,17 +19,28 @@ import pickle
 from nltk.corpus import gutenberg
 
 from languagemodeling.ngram import NGram
+# from languagemodeling.ngram import NGram, AddOneNGram, InterpolatedNGram
+
+
+# models = {
+#     'ngram': NGram,
+#     'addone': AddOneNGram,
+#     'inter': InterpolatedNGram,
+# }
 
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    sents = gutenberg.sents('austen-emma.txt')
+    # WORK HERE!! LOAD YOUR TRAINING CORPUS
+    sents = gutenberg.sents(['austen-emma.txt', 'austen-sense.txt'])
 
     # train the model
     n = int(opts['-n'])
     model = NGram(n, sents)
+    # model_class = models[opts['-m']]
+    # model = model_class(n, sents)
 
     # save it
     filename = opts['-o']
